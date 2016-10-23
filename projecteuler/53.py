3c0aa3d426542f87c124c5caeffcd28c2078d5c8
EnzoHideki <enzo25estrela@gmail.com>
2016-10-23 19:57:10 -0200

    prettifying code

--- projecteuler/53.py
+++ projecteuler/53.py
@@ -2,18 +2,13 @@ v = []
 const = 101
 
 def main():
-	l = [1]
-	for i in range(1,const):
-		l.append(0)
-
-	v.append(l)
+	v.append([1,0])
 
 	for i in range(1,const):
 		linha = [1]
 		for j in range(1,i+1):
 			linha.append(v[i-1][j-1] + v[i-1][j])
-		for j in range(i+1,const):
-			linha.append(0)
+		linha.append(0)
 
 		v.append(linha)
 
