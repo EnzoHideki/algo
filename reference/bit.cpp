1e6ed05176c04ecec8d29e7ad31e809825e4a1e8
U-LAPTOP-52Q8AAHS\Acer <enzo.nakamura@usp.br>
2017-07-19 00:25:45 -0300

    improving bit

--- reference/bit.cpp
+++ reference/bit.cpp
@@ -1,3 +1,4 @@
+//1 - Point update Range query
 //iterative version
 void update(int i, int val) {
 	for (; i <= n; i += (i & -i)) bit[i] += val;
@@ -19,4 +20,12 @@ void update(int i, int val) {
 int sum(int i) {
 	if (i == 0) return 0;
 	return bit[i] + sum(i - (i & -i));
+}
+
+
+//2 - Range update Point query
+//Here, sum(i) is value at position i (sum of first i elements)
+void update(int l, int r, int val) {
+	update(l, val);
+	update(r+1, -val);
 }
