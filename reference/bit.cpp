8ef0ec3f2c95c1d4234272ef39a8541c78629cc6
Enzo H. Nakamura <enzo25estrela@gmail.com>
2017-09-14 11:52:45 -0300

    adding range update range query bit

--- reference/bit.cpp
+++ reference/bit.cpp
@@ -29,3 +29,38 @@ void update(int l, int r, int val) {
 	update(l, val);
 	update(r+1, -val);
 }
+
+
+//3 - Range update Range query
+int bit1[MAXN];
+int bit2[MAXN];
+
+void update1(int p, int v) {
+	for (; p <= n; p += p&(-p))
+		bit1[p] += v; 	 
+}
+
+void update2(int p, int v) {
+	for (; p <= n; p += p&(-p))
+		bit2[p] += v; 	 
+}
+
+// Add v to A[a...b] 
+void update(int a, int b, int v) {
+	update1(a, v);	
+	update1(b + 1, -v); 	
+	update2(a, v * (a-1)); 	
+	update2(b + 1, -v * b);
+}
+
+int sum(int bit, int b) {
+	int sum = 0; 	
+	for(; b > 0; b -= b & (-b))
+		sum += (bit == 1 ? bit1[b] : bit2[b]);
+	return sum;
+}
+
+// Return sum A[1...b]
+int sum(int b) {
+	return sum(1,b) * b - sum(2,b);
+}
