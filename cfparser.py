dc59b81a2aa2d0c6ee65873b07f569cbd9f01211
Enzo Hideki <enzo25estrela@gmail.com>
2018-07-25 00:38:55 -0300

    Now cfparser puts tests in 'tests' file, and is now compatible with check.py; Also supports gym contests

--- cfparser.py
+++ cfparser.py
@@ -5,32 +5,37 @@ import requests
 import sys
  
 if len(sys.argv) < 3:
-    print "Need contest number and problem"
+    print("Need contest number and problem")
     sys.exit(0)
 
 def parse_task():
-    s = requests.get("http://codeforces.com/contest/" + sys.argv[1] + 
+    if (int(sys.argv[1]) > 100000):
+        s = requests.get("http://codeforces.com/gym/" + sys.argv[1] + 
+                    "/problem/" + sys.argv[2]).text
+    else:    
+        s = requests.get("http://codeforces.com/contest/" + sys.argv[1] + 
                     "/problem/" + sys.argv[2]).text
 
     a = re.findall("Input</div><pre>.*?</pre>", s)
     b = re.findall("Output</div><pre>.*?</pre>", s)
  
     if (len(a) == 0):
-        print "Something went wrong..."
+        print("Something went wrong...")
         sys.exit(0)
  
     assert(len(a) == len(b))
 
-    for i in xrange(len(a)):
+    for i in range(len(a)):
         a[i] = a[i][16:-6].replace('<br />', '\n')
         b[i] = b[i][17:-6].replace('<br />', '\n')
  
-    for i in xrange(len(a)):
-        with open("in" + str(i+1), "w") as f:
-            print >> f, a[i]
-        with open("ans" + str(i+1), "w") as f:
-            print >> f, b[i]
+    with open("tests", "w") as f:
+        for i in range(len(a)):
+            print(a[i], end="", file=f)
+            print("ans:", file=f)
+            print(b[i], end="", file=f)
+            print("===", file=f)
 
-    print len(a), "tests parsed!"
+    print(len(a), "tests parsed!")
 
 parse_task()
